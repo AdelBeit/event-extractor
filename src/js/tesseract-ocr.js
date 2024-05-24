e61494f447c3ec257acb5a5672b6aec6ac43b427
Adele <adelbeit@gmail.com>
2024-05-24 03:35:07 -0700

    using scheudlers instead of single worker

--- src/js/tesseract-ocr.js
+++ src/js/tesseract-ocr.js
@@ -9,20 +9,47 @@ function startRecognize(img) {
 
 async function recognizeFiles(files) {
   document.getElementById("log").innerHTML = "";
-  const worker = await Tesseract.createWorker("eng", 1, {
-    logger: progressUpdate,
-  });
-  for (let i = 0; i < files.length; i++) {
-    let file = files[i];
-    const ret = await worker.recognize(file);
-    console.log("done", i, file, ret.data.text);
-    // try {
-    //   progressUpdate({ status: "done", data: ret.data });
-    // } catch (e) {
-    //   console.log(e);
-    // }
+
+  /* scheudler */
+  const scheduler = Tesseract.createScheduler();
+  const workerGen = async () => {
+    const worker = await Tesseract.createWorker("eng", 1);
+    scheduler.addWorker(worker);
+  };
+
+  const workerN = 2;
+  const resArr = Array(workerN);
+  for (let i = 0; i < workerN; i++) {
+    resArr[i] = workerGen();
   }
-  await worker.terminate();
+  await Promise.all(resArr);
+
+  const results = [];
+  for (let i = 0; i < files.length; i++) {
+    let result = scheduler.addJob("recognize", files[i]).then((ret) => {
+      console.log(i, ret.data.text);
+      return x;
+    });
+    results.push(result);
+  }
+  await Promise.all(results);
+  await scheduler.terminate();
+  return;
+
+  // const worker = await Tesseract.createWorker("eng", 1, {
+  //   logger: progressUpdate,
+  // });
+  // for (let i = 0; i < files.length; i++) {
+  //   let file = files[i];
+  //   const ret = await worker.recognize(file);
+  //   console.log("done", i, file, ret.data.text);
+  //   // try {
+  //   //   progressUpdate({ status: "done", data: ret.data });
+  //   // } catch (e) {
+  //   //   console.log(e);
+  //   // }
+  // }
+  // await worker.terminate();
 }
 
 function progressUpdate(packet) {
